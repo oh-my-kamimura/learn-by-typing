52cdfd1dab015da53dcf1f5890f2ca5c5486fc76
yooohei03 <76262083+yooohei03@users.noreply.github.com>
2023-05-06 16:13:08 +0900

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lbt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'learn-by-typing-app';
+}
