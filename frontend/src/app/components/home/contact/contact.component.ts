43339051d27b38773fd5980f81c89ba2f5474566
yooohei03 <76262083+yooohei03@users.noreply.github.com>
2024-06-20 23:38:01 +0900

    clean: Divided into components

---
+++ frontend/src/app/components/home/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lbt-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss', '/src/styles.scss']
+})
+export class ContactComponent {
+
+}
--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'lbt-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ContactComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss', '/src/styles.scss']
 })
