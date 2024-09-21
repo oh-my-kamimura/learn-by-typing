c4f5e2e7f9ee6a06e5fe493ce023cdf6765d5985
yooohei03 <76262083+yooohei03@users.noreply.github.com>
2024-09-21 17:30:42 +0900

    add: Implement client-side processing

--- frontend/src/app/components/home/contact/contact.component.ts
+++ frontend/src/app/components/home/contact/contact.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MessageService } from '../../../services/message.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'lbt-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss', '/src/styles.scss']
+  styleUrls: ['./contact.component.scss', '/src/styles.scss'],
 })
 export class ContactComponent {
 
+  protected inquiryForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private messageService: MessageService) {
+    this.inquiryForm = this.fb.group({
+      subject: [''],
+      email  : ['', [Validators.email]],
+      message: ['', [Validators.required]]
+    });
+  }
+
+  sendMessage() {
+    if (this.inquiryForm.valid) {
+      // TODO: ここでAPIを送信
+      // this.messageService.sendMessage(this.inquiryForm.value)
+      console.log(this.inquiryForm.value);
+    } else {
+      console.log('フォームが無効です');
+    }
+  }
 }
