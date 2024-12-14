d41c787d2969af06ea13812e5f0cf452a4e3046b
yooohei03 <76262083+yooohei03@users.noreply.github.com>
2024-12-14 15:28:15 +0900

    fix: Fixed a problem with annotations appearing in results

--- frontend/src/app/components/playing/playing.component.ts
+++ frontend/src/app/components/playing/playing.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { QuestionDataService } from '../../services/question-data.service';
 import { PlayingDataService } from '../../services/playing-data.service';
 import { BreadcrumbsComponent } from '../shared/breadcrumbs/breadcrumbs.component';
+import { EmielService } from '../../services/emiel.service';
 
 @Component({
   selector: 'lbt-playing',
   standalone: true,
-  imports: [RouterOutlet, BreadcrumbsComponent],
+  imports: [RouterOutlet, CommonModule, BreadcrumbsComponent],
   templateUrl: './playing.component.html',
   styleUrls: ['./playing.component.scss', '/src/styles.scss'],
 })
@@ -20,7 +22,8 @@ export class PlayingComponent {
   constructor(
     private route: ActivatedRoute,
     public questionDataService: QuestionDataService,
-    public playingDataService: PlayingDataService
+    public playingDataService: PlayingDataService,
+    public emielService: EmielService,
   ) {}
 
   ngOnInit(): void {
